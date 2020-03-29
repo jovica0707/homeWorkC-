52cc31fb7875fc84557dbb4921e2fc5adcb7f02d
jovica0707 <58665696+jovica0707@users.noreply.github.com>
2020-03-29 23:46:47 +0200

    Add files via upload

---
+++ homeWork3new/homeWork3.1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        
+       //////Isto taka i za ovaa mi trebashe pomosh mislev deka pomalce treba da se kuca kod ama iskochi deka poveke treba
+
+        static int Sum(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        static int Difference(int num1, int num2)
+        {
+            int diff = num1 - num2;
+            return diff;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter - or +");
+                bool operationSucces = char.TryParse(Console.ReadLine(), out char operation);
+
+
+                if (!operationSucces || operation != '-' && operation != '+')
+                {
+                    Console.WriteLine("Invalid");
+                    continue;
+                }
+                int input1;
+
+                while (true)
+                {
+                    Console.WriteLine("Enter the first number");
+                    bool num1Success = int.TryParse(Console.ReadLine(), out input1);
+
+                    if (!num1Success)
+                    {
+                        Console.WriteLine("Invalid input");
+                        continue;
+                    }
+                    break;
+                }
+
+                int input2;
+
+                while (true)
+                {
+                    Console.WriteLine("Enter the second number");
+                    bool num2Success = int.TryParse(Console.ReadLine(), out input2);
+
+                    if (!num2Success)
+                    {
+                        Console.WriteLine("Invalid input");
+                        continue;
+                    }
+                    break;
+                }
+
+                switch (operation)
+                {
+                    case '+':
+                        Console.WriteLine($"The sum of {input1} and {input2} is {Sum(input1, input2)}");
+                        break;
+                    case '-':
+                        Console.WriteLine($"The difference of {input1} and {input2} is {Difference(input1, input2)}");
+                        break;
+                }
+                break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homeWork3new/homeWork3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Substrings(int num)
+        {
+            string sedc = "Hello from SEDC Codecademy v7.0";
+
+            if (num > sedc.Length)
+            {
+                Console.WriteLine($"Input a number lower than {sedc.Length}");
+
+            }
+            else
+            {
+                string sedcNew = sedc.Substring(0, num);
+
+                Console.WriteLine($"The fist characters from the new string are '{sedcNew}' and their length is {sedcNew.Length}");
+            }
+
+
+        }
+        
+        /////Do tuka mi e jasno za podole reshenieto, pobarav pomosh 
+      
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number:");
+                bool Sucenssn = int.TryParse(Console.ReadLine(), out int n);
+
+                if (Sucenssn)
+                {
+                    Substrings(n);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
