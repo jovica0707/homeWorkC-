6a63f6ddef5da5783581966dd983682bbe9776cc
jovica0707 <58665696+jovica0707@users.noreply.github.com>
2020-04-10 00:15:00 +0200

    Add files via upload

---
+++ vezbaChetvrtok09042020/task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] G1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] G2 = new string[] { "Cvetko", "Darko", "Mete", "Ante" };
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter numer of group:");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    foreach (var student in G1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    continue;
+                }
+                else if (input == "2")
+                {
+                    foreach (var student in G2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    continue;
+                }
+                else
+
+                {
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
