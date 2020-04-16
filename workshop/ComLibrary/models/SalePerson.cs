163cbf34791ed4a13ccc1f55e020faeae939d645
jovica0707 <58665696+jovica0707@users.noreply.github.com>
2020-04-16 21:51:13 +0200

    Add files via upload

---
+++ workshop/ComLibrary/models/SalePerson.cs
@@ -0,0 +1,44 @@
+﻿using ComLibrary.Enums;
+using ComLibrary.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+public class SalePerson : Employee
+{
+    private double SeccessSaleRavenue { get; set; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int Salary { get; }
+    public RoleType Role { get; }
+
+    public SalePerson(string first, string last)
+    {
+        FirstName = first;
+        LastName = last;
+        Salary = 500;
+        Role = RoleType.Sales;
+    }
+    public void ExtenndSuccessSaleRevenue(int successValue)
+    {
+        SeccessSaleRavenue += successValue;
+    }
+    public override double GetSalary()
+    {
+        if (SeccessSaleRavenue <= 2000)
+        {
+            return Salary + 500;
+        }
+        else if (SeccessSaleRavenue <= 5000)
+        {
+            return Salary + 800;
+        }
+        else if (SeccessSaleRavenue > 5000)
+        {
+            return Salary + 2000;
+        }
+        else
+        {
+            return Salary;
+        }
+    }
+}
